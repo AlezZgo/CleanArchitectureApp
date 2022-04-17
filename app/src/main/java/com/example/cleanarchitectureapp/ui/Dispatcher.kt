c0ce2d3d40a4f04747780e5ef8bd6aae29772abf
Alexander <alez.grante@gmail.com>
2022-04-17 20:07:38 +0300

    Dispatcher.kt

---
+++ app/src/main/java/com/example/cleanarchitectureapp/ui/Dispatcher.kt
@@ -0,0 +1,28 @@
+package com.example.cleanarchitectureapp.ui
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+interface Dispatcher  {
+
+    fun doUi(scope: CoroutineScope, block: suspend () -> Unit)
+
+    fun doBackground(scope: CoroutineScope,block: suspend () -> Unit)
+
+    class Base : Dispatcher {
+
+        override fun doBackground(scope: CoroutineScope,block: suspend () -> Unit) {
+            scope.launch(Dispatchers.IO) {
+                block.invoke()
+            }
+        }
+
+        override fun doUi(scope: CoroutineScope,block: suspend () -> Unit) {
+            scope.launch(Dispatchers.Main) {
+                block.invoke()
+            }
+        }
+
+    }
+}
