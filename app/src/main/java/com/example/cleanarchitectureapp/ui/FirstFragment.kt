aae670abbb25b2b5cc026f9a7e30d3bb6b9cbf58
Alexander <alez.grante@gmail.com>
2022-04-17 17:36:30 +0300

    SharedViewModel.kt

--- app/src/main/java/com/example/cleanarchitectureapp/ui/FirstFragment.kt
+++ app/src/main/java/com/example/cleanarchitectureapp/ui/FirstFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.cleanarchitectureapp.R
+import com.example.cleanarchitectureapp.databinding.FragmentFirstBinding
+import com.example.cleanarchitectureapp.databinding.FragmentSecondBinding
 
 class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/cleanarchitectureapp/ui/SecondFragment.kt
+++ app/src/main/java/com/example/cleanarchitectureapp/ui/SecondFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.cleanarchitectureapp.R
+import com.example.cleanarchitectureapp.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = FragmentSecondBinding.inflate(inflater)
+        return binding.root
     }
 }
